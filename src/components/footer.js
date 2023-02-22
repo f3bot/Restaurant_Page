c3e575c194513b87abebeeb027d44cf8cabac8b4
f3bot <febot@protonmail.com>
2023-02-22 23:25:11 +0100

    Footer update
    Footer done, need to work on responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,6 +12,13 @@ const generateFooterContent = (parent) =>{
     const childSpan = document.createElement('span')
     childSpan.textContent = '©f3bot'
     parent.appendChild(childSpan)
+    const a = document.createElement('a')
+    const img = document.createElement('img')
+    a.classList.add('link')
+    img.src = '../src/assets/github3.png'    
+    a.appendChild(img)
+    a.href = 'https://github.com/f3bot'
+    parent.appendChild(a)
 }
 
 export { generateFooter }
