8789d150a95092f36a487709d752d5f230a1dbf6
f3bot <febot@protonmail.com>
2023-02-22 23:12:52 +0100

    Footer Update
    Styled so it stays at the bottom of the page, need to add github href.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,16 @@ import '../styles/footer.css'
 
 const generateFooter = () =>{
     const div = document.createElement('div')
+    div.classList.add('footer')
     generateFooterContent(div)
     document.body.appendChild(div)
+    console.log('dhiv')
 }
 
 const generateFooterContent = (parent) =>{
     const childSpan = document.createElement('span')
     childSpan.textContent = '©f3bot'
-    parent.appendChild(childDiv)
+    parent.appendChild(childSpan)
 }
 
 export { generateFooter }
